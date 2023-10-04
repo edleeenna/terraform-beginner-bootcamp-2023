0aba8534e84dd4809eb7b15c63cbdd8e1aa9de6a
Elena D <edleeenna@hotmail.com>
2023-10-04 02:33:39 +0000

    #21 change structure of root module and add taggin to bucket

--- main.tf
+++ main.tf
@@ -1,34 +1,3 @@
-terraform {
-  cloud {
-    organization = "edleeennaorg"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-   required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.18.1"
-    }
-  }
-}
-
- 
-
-
-provider "aws" {
-  # Configuration options
-}
-
-provider "random" {
-  # Configuration options
-}
-
 resource "random_string" "bucket_name" {
    length  = 32
    special = false
@@ -42,11 +11,7 @@ resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
 
   tags = {
-    Name = "My bucket"
-    Environment = "Dev"
+   UserUuid = var.user_uuid
+   Environment = "Dev"
   }
-}
-
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
 }
