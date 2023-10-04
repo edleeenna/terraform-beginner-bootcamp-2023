9291a9b3d086a8674d5ee634e580c6ef44c7dfaf
Elena D <edleeenna@hotmail.com>
2023-10-04 04:56:30 +0000

    #24 commit changes

--- variables.tf
+++ variables.tf
@@ -7,3 +7,14 @@
     error_message = "Invalid user UUID format. Please provide a valid UUID."
   }
 }
+
+variable "bucket_name" {
+  description = "AWS S3 Bucket Name"
+  type        = string
+
+  validation {
+    condition     = can(regex("^[a-z0-9.-]{3,63}$", var.bucket_name))
+    error_message = "Invalid bucket name. Bucket names must be between 3 and 63 characters and can only contain lowercase letters, numbers, hyphens, and periods. They must start and end with a lowercase letter or number."
+  }
+}
+
