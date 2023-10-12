fa8afce36d7ae103d38705044d84c0b9d219ddb0
Elena D <edleeenna@hotmail.com>
2023-10-12 00:58:03 +0000

    #47 test our terra home on terratowns test town

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -8,15 +8,15 @@
   }
 }
 
-variable "bucket_name" {
-  description = "AWS S3 Bucket Name"
-  type        = string
-
-  validation {
-    condition     = can(regex("^[a-z0-9.-]{3,63}$", var.bucket_name))
-    error_message = "Invalid bucket name. Bucket names must be between 3 and 63 characters and can only contain lowercase letters, numbers, hyphens, and periods. They must start and end with a lowercase letter or number."
-  }
-}
+#variable "bucket_name" {
+#  description = "AWS S3 Bucket Name"
+#  type        = string
+#
+#  validation {
+#    condition     = can(regex("^[a-z0-9.-]{3,63}$", var.bucket_name))
+#    error_message = "Invalid bucket name. Bucket names must be between 3 and 63 characters and can only contain lowercase letters, numbers, hyphens, and periods. They must start and end with a lowercase letter or number."
+#  }
+#}
 
 variable "index_html_filepath" {
   description = "File path to the index.html file"
