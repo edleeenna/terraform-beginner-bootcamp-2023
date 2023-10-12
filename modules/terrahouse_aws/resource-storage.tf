34ee133bd826d8aeefa726447ec4b7048c835ca9
Elena D <edleeenna@hotmail.com>
2023-10-12 10:58:30 +1000

    #47 test our terra home on terratowns test town (#48)

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -1,6 +1,6 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs
 resource "aws_s3_bucket" "website_bucket" {
-  bucket = var.bucket_name
+  #bucket = var.bucket_name
 
   tags = {
    UserUuid = var.user_uuid
