017ec49fc32beb24a4654b4cfc56600ae414390d
Elena D <edleeenna@hotmail.com>
2023-10-13 05:39:56 +0000

    #51 finalise terrahome, update index.html, add styles and content

--- main.tf
+++ main.tf
@@ -29,29 +29,30 @@ module "home_one_hosting" {
 } 
 
 resource "terratowns_home" "home_one" {
-  name = "How to test your Terratown!!"
+  name = "Why you should watch The Bear!"
   description = <<DESCRIPTION
-I am testing my deploy to terratowns in Missingo
+A young chef from the fine dining world returns to Chicago to run his family's sandwich shop. Carmen Berzatto,
+ a brilliant young chef from the fine-dining world is forced to return home to run his local family sandwich shop - the Original Beef of Chicagoland - after a heartbreaking death in his family.
   DESCRIPTION
   domain_name = module.home_one_hosting.domain_name
-  town = "missingo"
+  town = "video-valley"
   content_version =  var.home_one.content_version
 }
 
-module "home_two_hosting" {
-  source = "./modules/terrahome_aws"
-  user_uuid = var.teacherseat_user_uuid
-  public_path = var.home_two.public_path
-  content_version = var.home_two.content_version
-} 
+#module "home_two_hosting" {
+#  source = "./modules/terrahome_aws"
+#  user_uuid = var.teacherseat_user_uuid
+#  public_path = var.home_two.public_path
+#  content_version = var.home_two.content_version
+#} 
+#
 
-
-resource "terratowns_home" "home_two" {
-  name = "Testing Home 2"
-  description = <<DESCRIPTION
-Test Home 2
-  DESCRIPTION
-  domain_name = module.home_two_hosting.domain_name
-  town = "missingo"
-  content_version =  var.home_two.content_version
-}
+#resource "terratowns_home" "home_two" {
+#  name = "Testing Home 2"
+#  description = <<DESCRIPTION
+#Test Home 2
+#  DESCRIPTION
+#  domain_name = module.home_two_hosting.domain_name
+#  town = "missingo"
+#  content_version =  var.home_two.content_version
+#}
